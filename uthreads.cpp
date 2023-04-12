86d6f958a27a0932dc418fdbda894607df681549
suchetzky <tal.suchetzky@mail.huji.ac.il>
2023-04-12 10:46:13 +0300

    first

---
+++ uthreads.cpp
@@ -0,0 +1,60 @@
+//
+// Created by talsu on 12/04/2023.
+//
+#include "uthreads.h"
+#include <iostream>
+using namespace std;
+
+int uthread_init(int quantum_usecs)
+{
+
+}
+
+int uthread_terminate(int tid)
+{
+
+}
+
+
+int uthread_block(int tid)
+{
+
+}
+
+int uthread_resume(int tid)
+{
+
+}
+
+int uthread_spawn(thread_entry_point entry_point)
+{
+
+}
+
+
+int uthread_sleep(int num_quantums)
+{
+
+}
+
+
+int uthread_get_tid()
+{
+
+}
+
+int uthread_get_total_quantums()
+{
+
+}
+
+int uthread_get_quantums(int tid)
+{
+
+}
+
+int main ( )
+{
+  cout << "Hello World! Tal";
+  return 0;
+}
